0b92b0e821ec98d24b72b7b076097f59492b7345
nargok <nargok@gmail.com>
2018-11-04 16:26:03 +0900

    axiosを導入

--- pages/index.vue
+++ pages/index.vue
@@ -26,6 +26,11 @@
 import AppLogo from '~/components/AppLogo.vue'
 
 export default {
+  async mounted() {
+    console.log(
+      JSON.stringify(await this.$axios.$get('https://qiita.com/api/v2/items?query=tag:nuxt.js'), true, ' ')
+    )
+  },
   components: {
     AppLogo
   }
